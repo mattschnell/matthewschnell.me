b4a286ebab2a64db9ee3cc6011df84fdfa8c1f89
mattschnell <54320446+mattschnell@users.noreply.github.com>
2020-07-15 00:37:48 -0400

    late

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 1;
+
+function tick() {
+    $('#main').html(`You have been on this page for ${num} seconds`);
+    num += 1;
+}
+
+setInterval(tick, 1000);
