0422f90c3bd72bd86346cc716291a98332df1145
mattschnell <54320446+mattschnell@users.noreply.github.com>
2020-07-16 14:18:20 -0400

    second attempt

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './style.scss';
 let num = 1;
 
 function tick() {
-    $('#main').html(`You have been on this page for ${num} seconds`);
-    num += 1;
+  $('#main').html(`You have been on this page for ${num} seconds`);
+  num += 1;
 }
 
 setInterval(tick, 1000);
